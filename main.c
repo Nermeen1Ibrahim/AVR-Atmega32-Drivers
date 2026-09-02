c46aeb52b98e6e066495d526ee6a05568561baee
Nermeen1Ibrahim <nermeenelfawakhry27@gmail.com>
2026-09-02 15:08:30 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,36 @@
 /*
- * GccApplication3.c
+ * GccApplication4.c
  *
- * Created: 9/2/2026 4:35:23 AM
+ * Created: 9/2/2026 5:26:12 AM
  * Author : dell
  */ 
-
-#define F_CPU 8000000UL
-#include <util/delay.h>
+# define F_CPU 8000000UL
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-#include "DIO_int.h"
-#include "LED_int.h"
+
+#include "DIO_int.h"             
+#include "SevenSegment_int.h"
+
+#include <util/delay.h>
 
 int main(void)
 {
-	
-	DIO_voidIntinitialization();
-	
-	LED_voidInit(DPORTB, PIN0);
+	SevSegment_voidInit();
+
+	u8 Local_u8Counter = 0;
 
 	while(1)
 	{
-		LED_voidToggle(DPORTB, PIN0);
+		
+		SevSegment_voidSetValue(Local_u8Counter);
+		
 		_delay_ms(1000);
+
+		Local_u8Counter++;
+		if(Local_u8Counter > 9)
+		{
+			Local_u8Counter = 0;
+		}
 	}
 
 	return 0;
